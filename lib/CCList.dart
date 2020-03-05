e7538b086e813ebc3bf0dc7831f44d2469c5b8c6
Alex Yatseyko <alex.yatseikogmail.com>
2020-03-05 19:58:36 +0200

    loaded

--- lib/CCList.dart
+++ lib/CCList.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:cryptotrackerr/CCData.dart';
 
-class CCList extends StatelessWidget{
-  List<CCData> data  = [
-    CCData(name: 'Bitocin', symbol: 'BTC', rank: 1, price: 6000),
-    CCData(name: 'Bitocin', symbol: 'BTC', rank: 2, price: 6000),
-    CCData(name: 'Bitocin', symbol: 'BTC', rank: 3, price: 6000)
-  ];
+class CCList extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return CCListState();
+  }
+}
+
+class CCListState extends State<CCList>{
+  List<CCData> data = [];
+
+  List<Widget> _buildList(){
+    return data.map((CCData f) => ListTile(
+      title: Text(f.symbol),
+      subtitle: Text(f.name),
+      leading: CircleAvatar(
+        child: Text(f.rank.toString())
+      ),
+      trailing: Text('\$${f.price.toString()}')
+    )).toList();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -18,18 +32,9 @@ class CCList extends StatelessWidget{
         child: ListView(
           children: _buildList(),
         )
-      )
-    );
-  }
-
-  List<Widget> _buildList(){
-    return data.map((CCData f) => ListTile(
-      title: Text(f.symbol),
-      subtitle: Text(f.name),
-      leading: CircleAvatar(
-        child: Text(f.rank.toString())
+      ),floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh)
       ),
-      trailing: Text('\$${f.price.toString()}')
-    )).toList();
+    );
   }
 }
